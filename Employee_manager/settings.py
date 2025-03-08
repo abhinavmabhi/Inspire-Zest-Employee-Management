0cd1166c7a4e726e448c3f6377733ec2f641faaf
abhinavmabhi <dexmarketing192018@gmail.com>
2025-03-08 17:07:52 +0530

    final commit

--- Employee_manager/settings.py
+++ Employee_manager/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whiteNoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Employee_manager.urls'
