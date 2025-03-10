fe6505ec637a7b45f4666c9ef9ebefc7677030b8
abhinavmabhi <dexmarketing192018@gmail.com>
2025-03-10 11:50:48 +0530

    settingd changed

--- Employee_manager/settings.py
+++ Employee_manager/settings.py
@@ -46,7 +46,7 @@ AUTH_USER_MODEL = 'app.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Note the correct case
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -72,6 +72,9 @@ TEMPLATES = [
         },
     },
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 WSGI_APPLICATION = 'Employee_manager.wsgi.application'
 
@@ -124,6 +127,7 @@ USE_TZ = True
 
 
 
+# Static files configuration
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
--- Employee_manager/wsgi.py
+++ Employee_manager/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise  # Note the correct case
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Employee_manager.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
